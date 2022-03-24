0cb19c13051e03e6e3f3f090d905e22f0e431c3f
Yuriy Polishchuk <yr.polishchuk@gmail.com>
2022-03-24 17:19:03 +0200

    add welcome

---
+++ small_games/scripts/small_games.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from colorama import Fore
+
+def greet():
+    print(Fore.GREEN + 'Welcome to the Small Games!')
+
+def main():
+    greet()
+
+if __name__ == '__main__':
+    main()
